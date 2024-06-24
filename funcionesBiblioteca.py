4d49bba5122c5515a13edd8ecf42148e9d6651d7
ManuelADMN <ma.dzm@outlook.com>
2024-06-24 09:00:07 -0400

    agregarlibros

--- funcionesBiblioteca.py
+++ funcionesBiblioteca.py
@@ -4,4 +4,19 @@ libreria=[];
 def agregarLibros():
     print("\n// Ha seleccionado agregar libro\n");
     try:
-    titulo=input("\n/Ingrese el titulo del libro:  ")
+        titulo=input("\n/Ingrese el titulo del libro:  ");
+    except TypeError:
+        print("Ingrese un nombre válido..")
+    else:
+        try:
+            autor=input("\n/Ingrese el autor del libro:  ");
+        except TypeError:
+            print("Ingrese un nombre válido..")
+        else:
+            try:
+                titulo=input("\n/Ingrese el titulo del libro:  ");
+            except TypeError:
+                print("Ingrese un nombre válido..")
+            else:
+        
+
