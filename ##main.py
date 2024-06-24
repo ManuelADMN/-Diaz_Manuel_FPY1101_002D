23b504f0f371c1e7eaedcb2523c6b51bdefb6dd6
ManuelADMN <ma.dzm@outlook.com>
2024-06-24 09:22:55 -0400

    Actualizado

--- ##main.py
+++ ##main.py
@@ -11,8 +11,14 @@ print("Opcion numero 3: Modificar un Libro.");
 print("Opcion numero 4: Eliminar un Libro.");
 print("Opcion numero 5: Guardar coleccion en un Archivo.");
 print("Opcion numero 6: Salir del Programa.");
-opcion =int(input("Ingrese una opcion de las que se muestran: "));
-if opcion==1:
-    print("Usted ha seleccionado la opcion 1, Agregar un libro. ");
-    func.agregarLibros();
-
+try:
+    opcion =int(input("Ingrese una opcion de las que se muestran: "));
+except ValueError:
+    print("\nIngrese una opción válida\n")
+else:
+    if opcion==1:
+        print("Usted ha seleccionado la opcion 1, Agregar un libro. ");
+        func.agregarLibros();
+    if opcion==2:
+        print("Usted ha seleccionado la opcion 1, Agregar un libro. ");
+        func.agregarLibros();
